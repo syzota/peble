9b0bc546f1ce5c9302ad550e6b83707cae1a8327
ojer <himexsaa@gmail.com>
2026-05-28 17:49:28 +0800

    Update SSL configuration for MySQL connection

--- server.ts
+++ server.ts
@@ -15,9 +15,9 @@ const pool = mysql.createPool({
   database: process.env.MYSQL_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : undefined,
 });
 
 pool.getConnection()
